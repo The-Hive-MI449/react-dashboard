69e4aea95f8537536b7a7550d64340086b61d4a4
perzcars <perzcars@msu.edu>
2026-03-12 00:27:17 +0000

    created bee fact button

--- randomfactAPI/src/App.tsx
+++ randomfactAPI/src/App.tsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import beeFactsData from './beefacts.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState("")
+
+  const getRandomBeeFact = () => {
+    const facts = beeFactsData.beeFacts
+    const randomIndex = Math.floor(Math.random() * facts.length)
+    setFact(facts[randomIndex])
+  }
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Random Bee Facts</h1>
+      <div></div>
+      <h1>Random Bee Fact!</h1>
       <div className="card">
-        <button>
-          Get a random bee fact!
-        </button>
+        <button onClick={getRandomBeeFact}>Get a random bee fact!</button>
+        <p>{fact}</p>
+        <small>All bee facts were sourced from bestbees.com. Thank you!</small>
       </div>
     </>
   )
 }
 
 export default App
+
