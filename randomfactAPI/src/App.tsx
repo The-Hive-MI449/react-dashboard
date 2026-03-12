ddeabf5b1409d8f00014c3bda12048cc239a8be0
perzcars <perzcars@msu.edu>
2026-03-12 01:43:49 +0000

    creating bee shaped button, coloring

--- randomfactAPI/src/App.tsx
+++ randomfactAPI/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import beeFactsData from './beefacts.json'
+import BeeSVG from './buttonbee'
 
 function App() {
   const [fact, setFact] = useState("")
@@ -16,7 +17,10 @@ function App() {
       <div></div>
       <h1>Random Bee Fact!</h1>
       <div className="card">
-        <button onClick={getRandomBeeFact}>Get a random bee fact!</button>
+        <h3>Bee inquisitive! Click the bee for a fun fact about our pollinating friends.</h3>
+        <div onClick={getRandomBeeFact} style={{ cursor: 'pointer' }}>
+          <BeeSVG />
+        </div>
         <p>{fact}</p>
         <small>All bee facts were sourced from bestbees.com. Thank you!</small>
       </div>
